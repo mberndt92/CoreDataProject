2ef514f22dec43f0d6f0247a6e125ce646b0c8b1
mberndt92 <maximilian_berndt@icloud.com>
2023-04-03 07:36:50 +0900

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Maximilian Berndt on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
